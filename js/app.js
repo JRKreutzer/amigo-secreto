fae3192a5d39933dbbedc3efa436086f0bc1483a
JRKreutzer <jose.kreutzer@gmail.com>
2023-10-17 20:52:03 -0300

    Correções/Melhorias limpar lista-sorteio + includes

--- js/app.js
+++ js/app.js
@@ -11,30 +11,29 @@ function adicionar() {
     console.log(nome);
     //Verificar se o nome já não está na lista ou se o nome não esta vazio
     let textoNomes = document.getElementById("lista-amigos");
-    for (let i = 0; i < listaAmigos.length; i++) {
-        if (nome == listaAmigos[i] || nome == "") {
-            verificador = true;
-            alertaErroNome = true;
-        }      
-    }
-    if (alertaErroNome == true) {
+    
+    if (listaAmigos.includes(nome) || nome == "") {
         alert("Nome repetido/inválido!");
+        document.getElementById("nome-amigo").value = "";
+        return;
+    }      
+    
+    
+    // Mostrar a lista no campo Amigos incluídos
+    if (textoNomes.textContent == "") {
+            textoNomes.textContent = nome;
+    }
+    else {
+        textoNomes.textContent = textoNomes.textContent + ", " + nome;
     }
     // Adicionar a lista de amigos
-    if (verificador == false) {
-        // Mostrar a lista no campo Amigos incluídos
-        if (textoNomes.textContent == "") {
-            textoNomes.textContent = nome;
-        }
-        else {
-            textoNomes.textContent = textoNomes.textContent + ", " + nome;
-        }
-        listaAmigos.push(nome);
-    }
+    listaAmigos.push(nome);
+    
     document.getElementById("nome-amigo").value = "";
 }
 
 function sortear() {
+    document.getElementById("lista-sorteio").textContent = "";
     if (listaAmigos.length < 3) {
         alert("Número de nomes insuficiente para o sorteio, adicione pelo menos 3 amigos!");
     } else {
