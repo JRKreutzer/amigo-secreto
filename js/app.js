28fa3e04efe22d3fb5e385d5d976e7a417b5819b
JRKreutzer <jose.kreutzer@gmail.com>
2023-10-17 19:39:34 -0300

    Funções adicionar, sortear e reiniciar JS

---
+++ js/app.js
@@ -0,0 +1,56 @@
+let listaAmigos = [];
+let verificador = false;
+
+function adicionar() {
+
+    // Coletar o nome digitado
+    let nome = document.getElementById("nome-amigo").value;
+    console.log(nome);
+    //Verificar se o nome já não está na lista
+    for (let i = 0; i < listaAmigos.length; i++) {
+        if (nome == listaAmigos[i]) {
+            alert("Nome já adicionado!");
+            verificador = true;
+        }      
+    }
+    // Adicionar a lista de amigos
+    if (verificador == false) {
+        listaAmigos.push(nome);
+    }
+    // Mostrar a lista no campo Amigos incluídos
+    document.getElementById("lista-amigos").textContent = listaAmigos;
+    verificador = false;
+    document.getElementById("nome-amigo").value = "";
+}
+
+function sortear() {
+    // Embaralhar lista
+    console.log(listaAmigos);
+    let listaEmbaralhada = embaralharLista(listaAmigos);
+    console.log(listaEmbaralhada);
+    // 0>1 1>2 2>3 3>4 4>0
+    for (let i = 0; i < listaEmbaralhada.length; i++) {
+        let sorteioAmigos = document.getElementById("lista-sorteio");
+        if (i == listaEmbaralhada.length - 1) {
+            sorteioAmigos.innerHTML =  sorteioAmigos.innerHTML + `<p>${listaEmbaralhada[i]} -> ${listaEmbaralhada[0]}</p>`
+            console.log(`${listaEmbaralhada[i]} -> ${listaEmbaralhada[0]}`)
+        } else {
+            sorteioAmigos.innerHTML = sorteioAmigos.innerHTML + `<p>${listaEmbaralhada[i]} -> ${listaEmbaralhada[i + 1]}</p>`
+        console.log(`${listaEmbaralhada[i]} -> ${listaEmbaralhada[i + 1]}`)
+        }
+    }
+}
+
+// Função para embaralhar lista
+function embaralharLista(lista) {
+    for (let i = 0; i < lista.length; i++) {
+        let j = Math.floor(Math.random() * (i + 1));
+        [lista[i] , lista[j]] = [lista[j], lista[i]];
+    }
+    return lista;
+}
+
+function reiniciar() {
+    document.getElementById("lista-amigos").textContent = "";
+    document.getElementById("lista-sorteio").textContent = "";
+}
